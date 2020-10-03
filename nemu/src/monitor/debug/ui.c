b4442d13cc21b76f471a202705fa4c364f5da58d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 15:41:19 +0800

    >  compile NEMU 3019207450 hasuer18-04 Linux hasuer1804-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:41:19 up  6:19,  8 users,  load average: 0.14, 0.06, 0.02 2ada22f92b24274fbabcd59cdc788ede11e8c24f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -50,14 +50,14 @@ static int cmd_Sstep(char* args){
 }
 
 static int cmd_info(char* args){
-	printf("eax is 0x%x\n", cpu.eax);
-	printf("ecx is 0x%x\n", cpu.ecx);
-	printf("edx is 0x%x\n", cpu.edx);
-	printf("ebx is 0x%x\n", cpu.ebx);
-	printf("esp is 0x%x\n", cpu.esp);
-	printf("ebp is 0x%x\n", cpu.ebp); 
-	printf("esi is 0x%x\n", cpu.esi);
-	printf("edi is 0x%x\n", cpu.edi);
+	printf("Value of eax: 0x%x\n", cpu.eax);
+	printf("Value of ecx: 0x%x\n", cpu.ecx);
+	printf("Value of edx: 0x%x\n", cpu.edx);
+	printf("Value of ebx: 0x%x\n", cpu.ebx);
+	printf("Value of esp: 0x%x\n", cpu.esp);
+	printf("Value of ebp: 0x%x\n", cpu.ebp); 
+	printf("Value of esi: 0x%x\n", cpu.esi);
+	printf("Value of edi: 0x%x\n", cpu.edi);
 	printf("Value of eip: 0x%x\n", cpu.eip);
 	return 0;
 }
