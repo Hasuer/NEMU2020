d8715a44ccebb68e514ea6e0c073dbcb9cc8a47e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 14:56:32 +0800

    >  compile NEMU 3019207450 hasuer18-04 Linux hasuer1804-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:56:32 up  4:02,  1 user,  load average: 0.14, 0.07, 0.06 82704034fdba774950a8530d6d5a1d71b666ec0

--- nemu/src/cpu/exec/data-call/call-template.h
+++ nemu/src/cpu/exec/data-call/call-template.h
@@ -7,13 +7,10 @@ make_helper (concat(call_i_, SUFFIX))
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
 	reg_l (R_ESP) -= DATA_BYTE;
 	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
-	DATA_TYPE_S displacement = op_src->val;
-	print_asm("call %x",cpu.eip + 1 + len + displacement);
-	cpu.eip +=displacement;
+	DATA_TYPE_S offset = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + len + offset);
+	cpu.eip += offset;
 	return len + 1;
 }
-
-
-
 
 #include "cpu/exec/template-end.h"
